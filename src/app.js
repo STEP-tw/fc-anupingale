d19048c80e51e79f49e0bda1e684ba8a472aad4d
Annapurna Pingale <44020027+anupingale@users.noreply.github.com>
2019-01-16 14:32:00 +0530

    Introduced the basic server.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,16 @@
+const { readFile } = require("fs");
+
 const app = (req, res) => {
-  res.statusCode = 404;
-  res.end();
+	if (req.url) {
+		readFile("." + req.url, (err, content) => {
+			res.statusCode = 200;
+			res.write(content);
+			res.end();
+		});
+	} else {
+		res.statusCode = 404;
+		res.end();
+	}
 };
 
 // Export a function that can act as a handler
