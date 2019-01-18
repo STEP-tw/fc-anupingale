2dabc364cdb4dfd9317824fc3c653ceb6f3f0701
Annapurna Pingale <44020027+anupingale@users.noreply.github.com>
2019-01-18 13:23:11 +0530

    Removed accidentally added file.

--- src/temp.js
+++
@@ -1,9 +0,0 @@
-let fs=require("fs")
-fs.readFile("./user_comments.json",(err,data)=>{
- return data.split(",").reduce((acc,dat)=>{
- console.log(data,typeof(data));
-  acc.push
- (JSON.parse(dat));
- return acc;
-},[]);
-});
