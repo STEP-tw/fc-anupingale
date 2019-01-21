158665910fb30a0cf0a0f538b9a9f53b886628d3
Annapurna Pingale <44020027+anupingale@users.noreply.github.com>
2019-01-21 11:07:03 +0530

    Modified functionality for refreshing comments.

--- public/guest_book.js
+++ public/guest_book.js
@@ -1,12 +1,7 @@
 const updateComments = function() {
-	fetch("/guest_book.html")
-		.then(res => {
-			return res.text();
-		})
-		.then(doc => {
-			let id = document.getElementById("user_comments");
-			let newDoc = document.createElement("html");
-			newDoc.innerHTML = doc;
-			id.innerHTML = newDoc.getElementsByClassName("comments")[0].innerHTML;
+	fetch("/comments")
+		.then(res => res.text())
+		.then(refreshedComments => {
+			document.getElementById('user_comments').innerHTML = refreshedComments;
 		});
 };
--- src/app.js
+++ src/app.js
@@ -1,10 +1,26 @@
 const { parser, parseDetails } = require("./parser.js");
 const { Handler } = require("./framework.js");
-const commentDetails = require("../src/user_comments.json");
+const jsonPath = () => "./src/user_comments.json";
 const app = new Handler();
-const { readFile, writeFile } = require("fs");
+const {
+	readFile,
+	writeFile,
+	existsSync,
+	writeFileSync,
+	readFileSync
+} = require("fs");
 
 const publicPrefix = req => "./public" + req.url;
+let commentDetails;
+
+const read = function(req, res, next) {
+	if (!existsSync(jsonPath())) {
+		writeFileSync(jsonPath(), "[]", "utf8");
+	}
+	const content = readFileSync(jsonPath(), "utf8");
+	commentDetails = JSON.parse(content);
+	next();
+};
 
 const send = function(res, statusCode, content) {
 	res.statusCode = statusCode;
@@ -12,42 +28,48 @@ const send = function(res, statusCode, content) {
 	res.end();
 };
 
-const readContent = function(req, res, next) {
+const readContent = function(req, res) {
 	readFile(publicPrefix(req), (err, content) => {
-		if (err) send(res, 404, "File Not Found");
+		if (err) return send(res, 404, "File Not Found");
 		send(res, 200, content);
 	});
 };
 
-const appendContent = function(req, res, next) {
+const appendContent = function(req, res) {
 	readFile(publicPrefix(req), (err, content) => {
 		let comments = parser(commentDetails);
 		send(res, 200, content + comments);
 	});
 };
 
-const writeComment = function(req, res, next) {
+const writeComment = function(req, res) {
 	let content = "";
 	req.on("data", chunk => (content += chunk));
 	req.on("end", () => {
-		commentDetails.push(parseDetails(content));
+		commentDetails.unshift(parseDetails(content));
 		let newComments = JSON.stringify(commentDetails);
-		writeFile("./src/user_comments.json", newComments, "utf8", (err, data) => {
+		writeFile(jsonPath(), newComments, "utf8", (err, data) => {
 			if (err) console.log(err);
 			appendContent(req, res);
 		});
 	});
 };
 
-const readHomeContent = function(req, res, next) {
+const readHomeContent = function(req, res) {
 	readFile("./public/index.html", (err, content) => {
 		send(res, 200, content);
 	});
 };
 
+const getComments = (req, res) => {
+	send(res, 200, parser(commentDetails));
+};
+
+app.use(read);
 app.get("/", readHomeContent);
 app.post("/guest_book.html", writeComment);
 app.get("/guest_book.html", appendContent);
+app.get("/comments", getComments);
 app.use(readContent);
 
 module.exports = app.handler.bind(app);
